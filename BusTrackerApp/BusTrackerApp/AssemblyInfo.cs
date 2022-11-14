1021a0c924dfec906461d4ab1fd4e1741cc73c66
UnlimitedNicholas <ndeer-smith@newtech.coppellisd.com>
2022-11-14 17:22:28 -0600

    Font
    trying to get fonts to work

--- BusTrackerApp/BusTrackerApp/AssemblyInfo.cs
+++ BusTrackerApp/BusTrackerApp/AssemblyInfo.cs
@@ -1,3 +1,11 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+//using Xamarin.Forms;
+
+[assembly: ExportFont("Helvetica.ttc", Alias = "Helvetica")]
+
+//using Xamarin.Forms;
+
+[assembly: ExportFont("Comic-Sans-MS.ttf", Alias = "comicSans")]
