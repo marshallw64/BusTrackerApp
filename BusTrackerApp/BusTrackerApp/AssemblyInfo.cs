58709b8a48bfb5168b952564939e4285ec323609
UnlimitedNicholas <ndeer-smith@newtech.coppellisd.com>
2022-11-16 16:11:14 -0600

    More Font Stuff
    yeah

--- BusTrackerApp/BusTrackerApp/AssemblyInfo.cs
+++ BusTrackerApp/BusTrackerApp/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Helvetica.ttc", Alias = "Helvetica")]
 [assembly: ExportFont("Comic Sans MS.ttf", Alias = "comicSans")]
-[assembly: ExportFont("Wingdings.ttf", Alias = "wingdings")]
 [assembly: ExportFont("Futura.ttc", Alias = "futura")]
 [assembly: ExportFont("Impact.ttf", Alias = "impact")]
+[assembly: ExportFont("Wingdings.ttf", Alias = "wingdings")]
