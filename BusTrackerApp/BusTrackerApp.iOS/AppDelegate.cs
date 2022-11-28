7d6e61bfa66135e28b5599a99fdfd21022149831
marshallw64 <78442083+marshallw64@users.noreply.github.com>
2022-11-28 15:55:19 -0600

    Added Maps (ios)

--- BusTrackerApp/BusTrackerApp.iOS/AppDelegate.cs
+++ BusTrackerApp/BusTrackerApp.iOS/AppDelegate.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using 
 using Foundation;
 using UIKit;
 
@@ -23,6 +23,8 @@ namespace BusTrackerApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
