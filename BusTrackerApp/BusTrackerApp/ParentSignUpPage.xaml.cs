e94bb8d01030aa774cca8e8f6c984e0187086ac7
UnlimitedNicholas <ndeer-smith@newtech.coppellisd.com>
2022-11-28 15:35:00 -0600

    Sign in Stuff
    Pop-up Confirmation

--- BusTrackerApp/BusTrackerApp/ParentSignUpPage.xaml.cs
+++ BusTrackerApp/BusTrackerApp/ParentSignUpPage.xaml.cs
@@ -11,6 +11,25 @@ namespace BusTrackerApp
         {
             InitializeComponent();
         }
+
+        void SendCode_Clicked(System.Object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(ParentSignUpEmail.Text);
+
+            if (isEmailEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new MapPage());
+            }
+
+            DisplayAlert("Success" ,"Your email has been sent", "Ok");
+
+
+            
+        }
     }
 }
 
