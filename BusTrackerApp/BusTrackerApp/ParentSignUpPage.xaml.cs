e4b695648ea50ddb7ee10bf0aa54d85e07c36cb5
UnlimitedNicholas <ndeer-smith@newtech.coppellisd.com>
2022-11-28 16:38:45 -0600

    More Navigation stuff

--- BusTrackerApp/BusTrackerApp/ParentSignUpPage.xaml.cs
+++ BusTrackerApp/BusTrackerApp/ParentSignUpPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
 namespace BusTrackerApp
@@ -15,17 +15,20 @@ namespace BusTrackerApp
         void SendCode_Clicked(System.Object sender, System.EventArgs e)
         {
             bool isEmailEmpty = string.IsNullOrEmpty(ParentSignUpEmail.Text);
-
+            //bool IsEmailActual = IsVa
             if (isEmailEmpty)
             {
-
+                DisplayAlert("Error", "No email in textbox. Please enter Email","Ok");
+                return;
             }
             else
             {
-                Navigation.PushAsync(new MapPage());
+                
+                DisplayAlert("Success", "Your email has been sent", "Ok");
+                Navigation.PushAsync(new CodePage());
             }
 
-            DisplayAlert("Success" ,"Your email has been sent", "Ok");
+           
 
 
             
