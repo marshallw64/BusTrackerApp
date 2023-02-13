b344fe9d922fe6303a06d23f8c7defce779772cb
marshallw64 <78442083+marshallw64@users.noreply.github.com>
2023-02-13 14:16:30 -0600

    AWS server communication set-up and works

--- BusTrackerApp/BusTrackerApp.Android/MainActivity.cs
+++ BusTrackerApp/BusTrackerApp.Android/MainActivity.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 using System.IO;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Android.Net;
 
 namespace BusTrackerApp.Droid
 {
@@ -21,6 +24,27 @@ namespace BusTrackerApp.Droid
             string dbName = "bus_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string fullPath = Path.Combine(folderPath, dbName);
+
+            var loggingConfig = AWSConfigs.LoggingConfig;
+            loggingConfig.LogMetrics = true;
+            loggingConfig.LogResponses = ResponseLoggingOption.Always;
+            loggingConfig.LogMetricsFormat = LogMetricsFormatOption.JSON;
+            loggingConfig.LogTo = LoggingOptions.SystemDiagnostics;
+
+            //var client = new AmazonDynamoDBClient(credentials, region);
+
+            /*
+             * Got Cliet Config from 
+             * https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/CodeSamples.DotNet.html
+             */
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            // This client will access the US East 1 region.
+            clientConfig.RegionEndpoint = RegionEndpoint.USEast1;
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient(clientConfig);
+
+            DynamoDBContext context = new DynamoDBContext(client);
+
+            AWSConfigs.AWSRegion = "us-east-2";
 
             LoadApplication(new App(fullPath));
         }
--- BusTrackerApp/BusTrackerApp/StudentLoginPage.xaml.cs
+++ BusTrackerApp/BusTrackerApp/StudentLoginPage.xaml.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.PlatformConfiguration;
+using static BusTrackerApp.DataTables.AWSConnection;
+using Amazon;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Endpoints;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.CredentialManagement;
+using System.IO.Compression;
+using System.IO;
 
 namespace BusTrackerApp
 {
@@ -12,6 +26,7 @@ namespace BusTrackerApp
         public StudentLoginPage()
         {
             InitializeComponent();
+            Console.WriteLine("Where am I?");
         }
 
         //checks that the ID is valid and sends student to the MapPage, displaying saved routes if the ID is assosiated with any
@@ -32,6 +47,81 @@ namespace BusTrackerApp
                 Navigation.PushAsync(new MapPage());
             }
         }
+
+        async void testRouteAWS_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (await Task.Run(saveItemToDB))
+            {
+                Console.Write("It worked yay!!!!!!!!!!!!!!!!!!");
+            } else
+            {
+                Console.Write("It didn't. Go back to fart");
+            }
+            
+        }
+
+        void WriteProfile(string profileName, string keyId, string secret)
+        {
+            Console.WriteLine($"Create the [{profileName}] profile...");
+            var options = new CredentialProfileOptions
+            {
+                AccessKey = keyId,
+                SecretKey = secret
+            };
+            var profile = new CredentialProfile(profileName, options);
+            var sharedFile = new SharedCredentialsFile();
+            sharedFile.RegisterProfile(profile);
+            Console.Write("New Profile Saved");
+        }
+
+        async Task<bool> saveItemToDB()
+        {
+            WriteProfile("default", "AKIA6LJNZIF7QCKALEUV", "DwEL+pPJTqpe6i+JtJmEoD3vdo28U+YDG/1FnXGD");
+
+            /*
+             * Got Cliet Config from 
+             * https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/CodeSamples.DotNet.html
+             */
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            // This client will access the US East 2 region.
+            clientConfig.RegionEndpoint = RegionEndpoint.USEast2;
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient(clientConfig);
+
+            DynamoDBContext context = new DynamoDBContext(client);
+
+            Dictionary<string, AttributeValue> attributes = new Dictionary<string, AttributeValue>();
+
+            //MemoryStream beeboo = new MemoryStream(132);
+
+            byte[] byteArray = { 0 };
+
+            MemoryStream beeboo = new MemoryStream(byteArray);
+
+            attributes["BusNum"] = new AttributeValue { N = "56" };
+            attributes["IsAM"] = new AttributeValue { B = beeboo };
+            attributes["driverLactionLat"] = new AttributeValue { N = "32.96945" };
+            attributes["driverLocationLon"] = new AttributeValue { N = "-96.99385" };
+
+
+            Console.WriteLine("I'm saving an item");
+
+            // Create PutItem request
+            PutItemRequest request = new PutItemRequest
+            {
+                TableName = "BusRoutes",
+                Item = attributes
+            };
+
+            Console.WriteLine("I'm still saving an item");
+            //Save method Document Model
+            //Issue PutItem request
+            var response = await client.PutItemAsync(request);
+            Console.WriteLine("entry saved");
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+
+
     }
 }
 
