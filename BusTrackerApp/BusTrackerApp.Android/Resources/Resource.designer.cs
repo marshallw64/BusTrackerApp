5e5619ed82ae32e98fb18402bf2f937e8ee8d884
UnlimitedNicholas <ndeer-smith@newtech.coppellisd.com>
2023-03-08 14:29:30 -0600

    78978ygyu

--- BusTrackerApp/BusTrackerApp.Android/Resources/Resource.designer.cs
+++ BusTrackerApp/BusTrackerApp.Android/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace BusTrackerApp.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.93")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		
